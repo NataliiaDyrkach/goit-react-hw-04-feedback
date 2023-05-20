f8781c199790efeac4057afe3429fd4e084f88c3
NataliiaDyrkach <n.dyrkach@gmail.com>
2023-05-20 12:59:15 +0300

    change to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
+import { useState } from 'react';
+import Statistics from './Statistics/Statistics';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Notification from './Notification/Notification';
+
+function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const clickButton = event => {
+    // приймається option це (event.target.name) з FeedbackOptions;
+    const option = event.target.name;
+
+    switch (option) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+
+      default:
+        console.log(`No option ${option}`);
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => good + bad + neutral;
+
+  const countPositiveFeedbackPercentage = () =>
+    `${Math.ceil((good * 100) / countTotalFeedback())}%`;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'bad', 'neutral']}
+          onLeaveFeedback={clickButton}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types';
+import css from './Section.module.css'
+
+function Section({title, children}) {
+    return (
+        <section className={css.section}>
+            {title && <h2 className={css.title}>{title}</h2>}
+            {children}
+        </section>
+    )
+}
+
+Section.propTypes = {
+    title: PropTypes.string,
+    children: PropTypes.element.isRequired,
+}
+
+export default Section;
+
